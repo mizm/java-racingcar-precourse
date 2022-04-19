f07696c82c45dee39922cef2eb28273c5b3b4ba3
marty.ni <marty.ni@kakaocorp.com>
2022-04-19 23:02:43 +0900

    Feat : Name 클래스 구현

---
+++ src/main/java/racingcar/domain/Name.java
@@ -0,0 +1,39 @@
+package racingcar.domain;
+
+import java.util.Objects;
+
+public class Name {
+
+    private static final String NAME_LENGTH_EXCEPTION_MESSAGE = "이름의 길이는 1~5자 입니다.";
+
+    private final String value;
+
+    public Name(String value) {
+        if (value.length() < 1 || value.length() > 5) {
+            throw new IllegalArgumentException(NAME_LENGTH_EXCEPTION_MESSAGE);
+        }
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(value, name1.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
