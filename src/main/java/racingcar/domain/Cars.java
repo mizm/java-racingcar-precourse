9aade902eee566ad4b06bff86fa6104cf34f3f4a
marty.ni <marty.ni@kakaocorp.com>
2022-04-21 11:24:43 +0900

    Feat : Cars winner 생성 로직 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 
 public class Cars {
 
+    private static final int COMPARE_VALUE = 0;
     private final List<Car> cars;
 
     private Cars(List<Car> cars) {
@@ -34,6 +35,40 @@ public class Cars {
 
     public List<Car> getCars() {
         return Collections.unmodifiableList(cars);
+    }
+
+    public Names winnerNames() {
+        Car winPosition = winnerPositionCar();
+        List<Name> names = new ArrayList<>();
+        for (Car car : cars) {
+            addNamesWithEqualPosition(winPosition, names, car);
+        }
+        return Names.from(names);
+    }
+
+    private Car winnerPositionCar() {
+        Car winner = null;
+        for (Car car : cars) {
+            winner = carWithHigherPosition(winner, car);
+        }
+    }
+
+    private Car carWithHigherPosition(Car winner, Car car) {
+        if (winner == null) {
+            return car;
+        }
+
+        if (winner.compareTo(car) < COMPARE_VALUE) {
+            return car;
+        }
+
+        return winner;
+    }
+
+    private void addNamesWithEqualPosition(Car winPosition, List<Name> names, Car car) {
+        if (car.equalsPosition(winPosition)) {
+            names.add(car.getName());
+        }
     }
 
     @Override
